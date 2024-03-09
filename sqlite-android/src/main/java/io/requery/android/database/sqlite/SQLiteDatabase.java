f434c72509dded0b7d8e37d2d143782dcfe0e37c
Truc Lam Nguyen <trucnguyenlam@gmail.com>
2024-03-09 21:54:50 +0000

    Load sqlite_extension

--- sqlite-android/src/main/java/io/requery/android/database/sqlite/SQLiteDatabase.java
+++ sqlite-android/src/main/java/io/requery/android/database/sqlite/SQLiteDatabase.java
@@ -87,9 +87,11 @@ public final class SQLiteDatabase extends SQLiteClosable implements SupportSQLit
      * Name of the compiled native library.
      */
     public static final String SIGNAL_TOKENIZER = "signal_tokenizer";
+    public static final String SQLITE_EXTENSION = "sqlite_extension";
     public static final String LIBRARY_NAME = "sqlite3x";
     static {
         System.loadLibrary(SIGNAL_TOKENIZER);
+        System.loadLibrary(SQLITE_EXTENSION);
         System.loadLibrary(LIBRARY_NAME);
     }
 
